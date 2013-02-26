4f829c98850c6a8c6c433c9c66faaee1045a5c3e
Demur Nodia <demur.nodia@gmail.com>
2013-02-27 01:54:40 +0400

    Issue4

--- src/edu/cst/webserver/http/Test.java
+++ src/edu/cst/webserver/http/Test.java
@@ -1,5 +1,7 @@
 package edu.cst.webserver.http;
 
+import edu.cst.webserver.env.ServerConfig;
+
 /**
  * @author Demur
  */
@@ -7,6 +9,15 @@ public class Test {
     public static void main(String[] args) {
         //Issue 2
         testIssue2();
+        //Issue 4
+        ServerConfig con = ServerConfig.getInstance();
+        if(con.isSupportedMimeType("text/html")){
+            System.out.println("True");
+        }else System.out.println("False");
+
+        if(con.isSupportedMethod("GET")){
+            System.out.println("True");
+        }else System.out.println("False");
     }
 
     private static void testIssue2() {
