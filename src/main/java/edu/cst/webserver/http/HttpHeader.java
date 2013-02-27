ebf0cd942fe46a3fec644b9985db83dc6689b256
Vaxtang Pataraia <v_pataraia@cu.edu.ge>
2013-02-28 01:00:43 +0400

    HttpHeader

---
+++ src/main/java/edu/cst/webserver/http/HttpHeader.java
@@ -0,0 +1,13 @@
+package edu.cst.webserver.http;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Sopho
+ * Date: 28.02.13
+ * Time: 0:54
+ * To change this template use File | Settings | File Templates.
+ */
+public class HttpHeader {
+
+
+}
---
+++ src/test/java/edu/cst/webserver/http/HttpHeaderTest.java
@@ -0,0 +1,12 @@
+package edu.cst.webserver.http;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Sopho
+ * Date: 28.02.13
+ * Time: 0:55
+ * To change this template use File | Settings | File Templates.
+ */
+public class HttpHeaderTest {
+
+}
