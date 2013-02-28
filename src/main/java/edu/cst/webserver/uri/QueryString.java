133d485fac381ad60c7b24dc06fb7edd9843961e
otar <otar.zakalashvili@gmail.com>
2013-02-28 12:16:46 -0800

    Imports optimized and cleaned. Issue #9

---
+++ src/main/java/edu/cst/webserver/uri/QueryString.java
@@ -0,0 +1,44 @@
+package edu.cst.webserver.uri;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Otar
+ * Date: 2/28/13
+ * Time: 1:15 AM
+ */
+public class QueryString {
+
+    /**
+     * getUrlParams returns key, value queryStringMap built from queryString
+     * String queryString is http query string
+     *
+     */
+    public static Map<String, String> getUrlParams(String queryString) {
+        Map<String, String> queryStringMap = new HashMap<String, String>();
+
+        for (String pairString : queryString.split("&")) {
+            String pairSplitted[] = pairString.split("=");
+            String key = "";
+            String value = "";
+            try {
+                key = URLDecoder.decode(pairSplitted[0], "UTF-8");
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+            try {
+                value = URLDecoder.decode(pairSplitted[1], "UTF-8");
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+            if (!key.isEmpty()) {
+                queryStringMap.put(key, value);
+            }
+        }
+        return queryStringMap;
+    }
+}
