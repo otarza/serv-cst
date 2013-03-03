4c4be7b6a7408ba6466f25f424a9f8314041af89
Vaxtang Pataraia <v_pataraia@cu.edu.ge>
2013-03-04 00:32:46 +0400

    #13

--- src/main/java/edu/cst/webserver/http/HttpRequestException.java
+++ src/main/java/edu/cst/webserver/http/HttpRequestException.java
@@ -3,12 +3,16 @@ package edu.cst.webserver.http;
 public class HttpRequestException extends Exception {
     private HttpStatus.Code httpStatusCode;
 
-    public HttpRequestException(HttpStatus.Code statusCode,String message)
-    {
-        this.httpStatusCode=statusCode;
-
+    public HttpRequestException(HttpStatus.Code statusCode, String message) {
+        super(message);
+        this.httpStatusCode = statusCode;
     }
-    public HttpStatus.Code getStatusCode(){
+
+    public HttpRequestException(HttpStatus.Code statusCode) {
+        this.httpStatusCode = statusCode;
+    }
+
+    public HttpStatus.Code getStatusCode() {
         return this.httpStatusCode;
 
     }
