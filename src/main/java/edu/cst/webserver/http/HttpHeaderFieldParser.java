28870a66ac5e105aa28a7f4bf4b186d357704830
otar <otar.zakalashvili@gmail.com>
2013-04-17 12:55:50 +0400

    HttpHeadderFieldParser headers list parser added, not tested. #29

--- src/main/java/edu/cst/webserver/http/HttpHeaderFieldParser.java
+++ src/main/java/edu/cst/webserver/http/HttpHeaderFieldParser.java
@@ -1,6 +1,8 @@
 package edu.cst.webserver.http;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -9,22 +11,22 @@ import java.util.Map;
 public class HttpHeaderFieldParser {
     //static US ASCII character lookup array
     private static char allowedHeaderFieldKeyChars[] = {
-            0,    0,    0,    0,    0,    0,    0,    0,
-            0,    0,    0,    0,    0,    0,    0,    0,
-            0,    0,    0,    0,    0,    0,    0,    0,
-            0,    0,    0,    0,    0,    0,    0,    0,
-            0,    '!',  '"',  '#',  '$',  '%',  '&',  '\'',
-            0,    0,    '*',  '+',  0,    '-',  '.',  0,
-            '0',  '1',  '2',  '3',  '4',  '5',  '6',  '7',
-            '8', '9',   0,    0,    0,    0,    0,    0,
-            0,   'A',   'B',  'C',  'D',  'E',  'F',  'G',
-            'H', 'I',   'J',  'K',  'L',  'M',  'N',  'O',
-            'P', 'Q',   'R',  'S',  'T',  'U',  'V',  'W',
-            'X', 'Y',   'Z',  0,     0,   0,    0,    '_',
-            '`', 'a',   'b',  'c',   'd', 'e',  'f',  'g',
-            'h', 'i',   'j',  'k',   'l', 'm',  'n',  'o',
-            'p', 'q',   'r',  's',   't', 'u',  'v',  'w',
-            'x', 'y',   'z',  0,     '|', 0,     '~'
+            0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0,
+            0, '!', '"', '#', '$', '%', '&', '\'',
+            0, 0, '*', '+', 0, '-', '.', 0,
+            '0', '1', '2', '3', '4', '5', '6', '7',
+            '8', '9', 0, 0, 0, 0, 0, 0,
+            0, 'A', 'B', 'C', 'D', 'E', 'F', 'G',
+            'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
+            'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
+            'X', 'Y', 'Z', 0, 0, 0, 0, '_',
+            '`', 'a', 'b', 'c', 'd', 'e', 'f', 'g',
+            'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+            'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
+            'x', 'y', 'z', 0, '|', 0, '~'
     };
 
     /**
@@ -33,7 +35,7 @@ public class HttpHeaderFieldParser {
      */
     public static Map<String, String> parse(String headerField) throws HttpRequestException {
         Map<String, String> headers = new HashMap<String, String>();
-        boolean isValid = false;
+        boolean isValid;
         if (headerField != null && !headerField.isEmpty()) {
             String tokens[] = headerField.trim().split(":\\s*", 2);
             if (tokens.length == 2 && tokens[0].length() > 0 && tokens[1].length() > 0 && hasValidCharacters(tokens[0])) {
@@ -51,6 +53,17 @@ public class HttpHeaderFieldParser {
             throw new HttpRequestException(HttpStatus.Code.BAD_REQUEST);
         }
         return headers;
+    }
+
+    public static List<Map<String, String>> parse_list(List<String> headers) throws HttpRequestException {
+        List<Map<String, String>> result = new ArrayList<Map<String, String>>();
+        for(int i =0; i<headers.size(); i++){
+            String header = headers.get(i);
+            Map<String, String> parsed = parse(header);
+            result.add(parsed);
+        }
+
+        return result;
     }
 
     private static boolean hasValidCharacters(String headerField) {
