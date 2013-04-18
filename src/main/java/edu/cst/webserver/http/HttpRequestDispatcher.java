ed5325947f07420c0fb926d1c3535350c3ba0a04
Demur Nodia <demur.nodia@gmail.com>
2013-04-18 16:34:48 +0400

    working on dispacher

--- src/main/java/edu/cst/webserver/http/HttpRequestDispatcher.java
+++ src/main/java/edu/cst/webserver/http/HttpRequestDispatcher.java
@@ -1,34 +1,26 @@
 package edu.cst.webserver.http;
 
+import edu.cst.webserver.http.handlers.HttpRequestDirectoryHandler;
+
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 /**
  * @author Demur
  */
 public class HttpRequestDispatcher implements HttpResponseErrorHandler{
-    String path;
+    Path path;
     File file;
-
-    public void HttpRequestDispatcher(){
-        this.path = "C:/path/to/folder/resource/1";
-        this.file = new File(path);
-
-        if (file.exists() && file.canRead()) {
-            if (file.isDirectory()) {
-                HttpDirFilesList v = new HttpDirFilesList(file);
-
-                String list = v.getDirFileList();
-
-                // vaxos klasi
-            } else if (file.isFile()) {
-                if (file.canExecute()) {
-                    // aq server side javascript
-                } else {
-//                    String mimeType = // demuris klasi romelic gveubneba mime tips
-                }
-            }
-        }
+    HttpResponse response;
+    HttpRequest request;
+    public HttpRequestDispatcher(HttpRequest request){
+        this.path = FileSystems.getDefault().getPath("C:/path/to/folder/resource/");
+        this.file = new File(path.toString());
+        this.request = request ;
+        response = new HttpResponseWrapper();
     }
 
     @Override
@@ -43,8 +35,52 @@ public class HttpRequestDispatcher implements HttpResponseErrorHandler{
         Something wrong happened!*/
     }
 
-    public HttpResponseWrapper dispatch() throws IOException {
-        HttpResponseWrapper response = new HttpResponseWrapper();
+    public HttpResponse dispatch() throws IOException {
+
+        if (file.exists() && file.canRead()) {
+            if (file.isDirectory()) {
+                HttpRequestHandler<File> handler = new HttpRequestDirectoryHandler(file,request,response);
+                try {
+                    handler.process(file, request, response);
+                } catch (HttpRequestException e) {
+                    // do something here
+                }
+//          return      new HttpRequestDirectoryHandler(file, request, response);
+
+                HttpDirFilesList v = new HttpDirFilesList(file);
+
+                String list = v.getDirList();
+
+                // vaxos klasi
+            } else if (file.isFile()) {
+                boolean isRegularExecutableFile = Files.isRegularFile(path) && Files.isReadable(path) && Files.isExecutable(path);
+                if (file.canExecute()) {
+
+
+                    // aq server side javascript
+//           return new HttpRequestJavaScriptHandler(file, request, response);
+                    //java executable
+
+//           return new HttpRequestJavaHandler(file, request, response);
+                } else {
+
+ /*                   HttpRequestHandler<File> handler = new HttpRequestFileHandler<File>();
+                    try {
+                        handler.process(file, request, response);
+                    } catch (HttpRequestException e) {
+                        // do something here
+                    }*/
+
+//             return       new HttpRequestFileHandler(file, request, response);
+//                    String mimeType = // demuris klasi romelic gveubneba mime tips
+                }
+            }
+        }
+
+
+
+
+    /*
         response.setErrorHandler(this);
 
         response.setHeader(HttpHeader.ACCEPT,"text/html");
@@ -53,7 +89,7 @@ public class HttpRequestDispatcher implements HttpResponseErrorHandler{
 
 
 
-        response.setStatus(HttpStatus.Code.OK);
+        response.setStatus(HttpStatus.Code.OK);*/
 
         return response;
     }
---
+++ src/main/java/edu/cst/webserver/http/handlers/HttpRequestDirectoryHandler.java
@@ -0,0 +1,58 @@
+package edu.cst.webserver.http.handlers;
+
+import edu.cst.webserver.env.MimeTypeDetector;
+import edu.cst.webserver.http.*;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.attribute.FileTime;
+import java.text.SimpleDateFormat;
+
+/**
+ * @author Demur
+ */
+public class HttpRequestDirectoryHandler implements HttpRequestHandler{
+
+    HttpRequest request;
+    HttpResponse response;
+    File file;
+    BasicFileAttributes attributes;
+    public HttpRequestDirectoryHandler(File file, HttpRequest request,HttpResponse response) throws IOException {
+        this.request = request;
+        this.response = response;
+        this.file = file;
+        attributes = Files.readAttributes(file.toPath(), BasicFileAttributes.class);
+    }
+
+    @Override
+    public InputStream getInputStream() throws IOException {
+        return new FileInputStream(file.getPath());
+    }
+
+    @Override
+    public String getContentType() throws IOException, HttpRequestException {
+      return MimeTypeDetector.detectMimeType(file.toString());
+    }
+
+    @Override
+    public int getContentLength() throws IOException {
+        return MimeTypeDetector.getContentLength(file);
+    }
+
+    @Override
+    public String getLastModified() {
+        FileTime fileModifyTime = attributes.lastModifiedTime();
+        return new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss 'GMT'").format(fileModifyTime);
+    }
+
+    @Override
+    public void process(Object handle, HttpRequest request, HttpResponse response) throws HttpRequestException {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
